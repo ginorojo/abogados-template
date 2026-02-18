02cb8b1fd79d60af12a32797d271f554d03ea225
ginorojo <ginorojoj@gmail.com>
2026-02-18 10:00:45 -0300

    cambios

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // <--- ESTO ES LA CLAVE
+  images: {
+    unoptimized: true, // Necesario para que las imágenes funcionen en Cloudflare
+  },
 };
 
 export default nextConfig;
